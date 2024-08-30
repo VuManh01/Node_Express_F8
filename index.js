6b495f14c9eac90a3b4697112d7f6e1d6ce1b124
VuManh01 <vutrongmanhmk@gmail.com>
2024-08-31 00:25:15 +0700

    \30.8.2024

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express(); //khởi tạo function
-const port = 3000;
-
-app.get("/tin-tuc", (req, res) => res.send("Hello"));
-
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "resources/view"));
+
+// Kiểm tra đường dẫn
+console.log("Views Directory:", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
