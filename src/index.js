84b858eaa8ebf97c921d0fca0ae05745a811a864
VuManh01 <vutrongmanhmk@gmail.com>
2024-09-03 00:23:04 +0700

    9.3.2024

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
-app.engine("hbs", handlebars({
-  extname: '.hbs'
-}));
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/view"));
 
